ac630c28b850f066d698b09b3889913823d9f935
huuchi207 <huuchi207@gmail.com>
2023-08-31 17:27:59 +0700

    coding

--- lib/Firebase/FirestoreData.g.dart
+++ lib/Firebase/FirestoreData.g.dart
@@ -8,10 +8,40 @@ part of 'FirestoreData.dart';
 
 FirestoreData _$FirestoreDataFromJson(Map<String, dynamic> json) =>
     FirestoreData(
-      restaurantName: json['restaurantName'] as String?,
+      json['restaurantName'] as String?,
+      json['overview'] == null
+          ? null
+          : SectionData.fromJson(json['overview'] as Map<String, dynamic>),
+      (json['menus'] as List<dynamic>?)
+          ?.map((e) => SectionData.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      (json['horizontalSlogans'] as List<dynamic>?)
+          ?.map((e) => SectionData.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      json['latitude'] as String?,
+      json['longitude'] as String?,
+      (json['openHours'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      json['phoneNumber'] as String?,
+      json['facebookLink'] as String?,
+      json['instagramLink'] as String?,
+      json['youtubeLink'] as String?,
+      json['privacyLink'] as String?,
+      json['aboutUsLink'] as String?,
     );
 
 Map<String, dynamic> _$FirestoreDataToJson(FirestoreData instance) =>
     <String, dynamic>{
       'restaurantName': instance.restaurantName,
+      'overview': instance.overview,
+      'menus': instance.menus,
+      'horizontalSlogans': instance.horizontalSlogans,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
+      'openHours': instance.openHours,
+      'phoneNumber': instance.phoneNumber,
+      'facebookLink': instance.facebookLink,
+      'instagramLink': instance.instagramLink,
+      'youtubeLink': instance.youtubeLink,
+      'privacyLink': instance.privacyLink,
+      'aboutUsLink': instance.aboutUsLink,
     };
---
+++ lib/Firebase/SectionData.g.dart
@@ -0,0 +1,22 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'SectionData.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SectionData _$SectionDataFromJson(Map<String, dynamic> json) => SectionData(
+      title: json['title'] as String?,
+      content: json['content'] as String?,
+      subContent: json['subContent'] as String?,
+      buttonTitle: json['buttonTitle'] as String?,
+    );
+
+Map<String, dynamic> _$SectionDataToJson(SectionData instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'content': instance.content,
+      'subContent': instance.subContent,
+      'buttonTitle': instance.buttonTitle,
+    };
